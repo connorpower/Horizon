8cef184eb4828d49513b3caa83940cc6b0bb15e8
Connor Power <connor.power@icloud.com>
2018-01-05 14:17:05 +0100

    Add first unit tests

---
+++ HorizonTests/FileTests.swift
@@ -0,0 +1,42 @@
+//
+//  FileTests.swift
+//  HorizonTests
+//
+//  Created by Connor Power on 05.01.18.
+//  Copyright © 2018 Semantical GmbH & Co. KG. All rights reserved.
+//
+
+import XCTest
+@testable import Horizon
+
+class FileTests: XCTestCase {
+
+    func testEquality1() {
+        let file1 = File(name: "My File", hash: nil)
+        let file2 = File(name: "My File", hash: nil)
+
+        XCTAssertTrue(file1 == file2)
+    }
+
+    func testEquality2() {
+        let file1 = File(name: "My File", hash: nil)
+        let file2 = File(name: "My Other File", hash: nil)
+
+        XCTAssertTrue(file1 != file2)
+    }
+
+    func testEquality3() {
+        let file1 = File(name: "My File", hash: "14D15E2B-8B5C-4404-96C6-67245A011903")
+        let file2 = File(name: "My Other File", hash: "14D15E2B-8B5C-4404-96C6-67245A011903")
+
+        XCTAssertTrue(file1 != file2)
+    }
+
+    func testEquality4() {
+        let file1 = File(name: "My File", hash: "14D15E2B-8B5C-4404-96C6-67245A011903")
+        let file2 = File(name: "My File", hash: "8C6692AA-DFF0-4CA4-84FD-8EFCF1D2FEBD")
+
+        XCTAssertTrue(file1 != file2)
+    }
+
+}

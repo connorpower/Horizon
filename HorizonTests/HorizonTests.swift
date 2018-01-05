a9f4e44f38395bb7f67f1a4886c1271d2bc6b52f
Connor Power <connor.power@icloud.com>
2018-01-05 14:13:26 +0100

    Add unit test scheme

---
+++ HorizonTests/HorizonTests.swift
@@ -0,0 +1,17 @@
+//
+//  HorizonTests.swift
+//  HorizonTests
+//
+//  Created by Connor Power on 05.01.18.
+//  Copyright © 2018 Semantical GmbH & Co. KG. All rights reserved.
+//
+
+import XCTest
+
+class HorizonTests: XCTestCase {
+
+    func testExample() {
+        XCTFail("Dummy failure")
+    }
+
+}

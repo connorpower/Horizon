a5d1bb97dff77c60d942a72c04dfe82e6c313a22
Connor Power <connor.power@icloud.com>
2018-01-12 18:13:43 +0100

    Mark dynamic strings as public in os_log calls

--- Horizon/API/IPFSWebserviceAPI.swift
+++ Horizon/API/IPFSWebserviceAPI.swift
@@ -16,16 +16,17 @@ struct IPFSWebserviceAPI: IPFSAPI {
     // MARK: File Management
 
     func add(file: URL, completion: @escaping ((_ response: AddResponse?, _ error: Error?) -> Void)) {
-        os_log("Adding file to IPFS: %s", log: Loggers.network, type: .info, file.absoluteString)
+        os_log("Adding file to IPFS: %{public}s", log: Loggers.network, type: .info, file.absoluteString)
 
         DefaultAPI.add(file: file) { (response, error) in
             if let response = response {
-                os_log("Added file %s to IPFS with hash %s, size %s", log: Loggers.network, type: .info,
+                os_log("Added file %{public}s to IPFS with hash %{public}s, size %{public}s",
+                       log: Loggers.network, type: .info,
                        response.name ?? "[no name]",
                        response.hash ?? "[no hash]",
                        response.size ?? "[no size]")
             } else {
-                os_log("Failed to add file %s to IPFS. %s", log: Loggers.network, type: .error,
+                os_log("Failed to add file %{public}s to IPFS. %{public}s", log: Loggers.network, type: .error,
                        self.describeError(error))
             }
 
@@ -34,14 +35,14 @@ struct IPFSWebserviceAPI: IPFSAPI {
     }
 
     func cat(arg: String, completion: @escaping ((_ data: Data?, _ error: Error?) -> Void)) {
-        os_log("Catting object from IPFS: %s", log: Loggers.network, type: .info, arg)
+        os_log("Catting object from IPFS: %{public}s", log: Loggers.network, type: .info, arg)
 
         DefaultAPI.cat(arg: arg) { (data, error) in
             if data != nil {
-                os_log("Cat of %s from IPFS returned %d bytes", log: Loggers.network, type: .info,
+                os_log("Cat of %{public}s from IPFS returned %d bytes", log: Loggers.network, type: .info,
                        data?.count ?? 0)
             } else if let error = error {
-                os_log("Failed to cat %s from IPFS. %s", log: Loggers.network, type: .error,
+                os_log("Failed to cat %{public}s from IPFS. %{public}s", log: Loggers.network, type: .error,
                        self.describeError(error))
             }
 
@@ -53,15 +54,16 @@ struct IPFSWebserviceAPI: IPFSAPI {
 
     func publish(arg: String, key: String?,
                  completion: @escaping ((_ response: PublishResponse?, _ error: Error?) -> Void)) {
-        os_log("Publishing file %s under key %s", log: Loggers.network, type: .info, arg, key ?? "[Node's Own PeerID]")
+        os_log("Publishing file %{public}s under key %{public}s", log: Loggers.network, type: .info,
+               arg, key ?? "[Node's Own PeerID]")
 
         DefaultAPI.publish(arg: arg, key: key) { (response, error) in
             if let response = response {
-                os_log("Published file with name %s under key %s", log: Loggers.network, type: .info,
+                os_log("Published file with name %{public}s under key %{public}s", log: Loggers.network, type: .info,
                        response.name ?? "[no name]",
                        response.value ?? "[no value]")
             } else {
-                os_log("Failed to publish file %s. %s", log: Loggers.network, type: .error,
+                os_log("Failed to publish file %{public}s. %{public}s", log: Loggers.network, type: .error,
                        arg, self.describeError(error))
             }
 
@@ -71,14 +73,14 @@ struct IPFSWebserviceAPI: IPFSAPI {
 
     func resolve(arg: String, recursive: Bool?,
                  completion: @escaping ((_ response: ResolveResponse?, _ error: Error?) -> Void)) {
-        os_log("Resolving hash %s", log: Loggers.network, type: .info, arg)
+        os_log("Resolving hash %{public}s", log: Loggers.network, type: .info, arg)
 
         DefaultAPI.resolve(arg: arg, recursive: recursive) { (response, error) in
             if let response = response {
-                os_log("Resolved hash %s to path %s", log: Loggers.network, type: .info,
+                os_log("Resolved hash %{public}s to path %{public}s", log: Loggers.network, type: .info,
                        arg, response.path ?? "[no path]")
             } else {
-                os_log("Failed to resolve hash %s", log: Loggers.network, type: .error, arg)
+                os_log("Failed to resolve hash %{public}s", log: Loggers.network, type: .error, arg)
             }
 
             completion(response, error)
@@ -87,13 +89,15 @@ struct IPFSWebserviceAPI: IPFSAPI {
 
     func keygen(arg: String, type: DefaultAPI.ModelType_keygen, size: Int32,
                 completion: @escaping ((_ response: KeygenResponse?, _ error: Error?) -> Void)) {
-        os_log("Generating key %s of type %s, size %d", log: Loggers.network, type: .info, arg, type.rawValue, size)
+        os_log("Generating key %{public}s of type %{public}s, size %d", log: Loggers.network, type: .info,
+               arg, type.rawValue, size)
 
         DefaultAPI.keygen(arg: arg, type: type, size: size) { (response, error) in
             if let response = response {
-                os_log("Generated key %s with ID %s", log: Loggers.network, type: .info, arg, response.id ?? "[no ID]")
+                os_log("Generated key %{public}s with ID %{public}s", log: Loggers.network, type: .info,
+                       arg, response.id ?? "[no ID]")
             } else {
-                os_log("Failed to generate key %s. %s", log: Loggers.network, type: .error,
+                os_log("Failed to generate key %{public}s. %{public}s", log: Loggers.network, type: .error,
                        arg, self.describeError(error))
             }
 
@@ -109,7 +113,8 @@ struct IPFSWebserviceAPI: IPFSAPI {
                 let keys = (response.keys ?? []).map({"\($0.name ?? "[no name]"): \($0.id ?? "[no ID]")"})
                 os_log("Found keypairs: ", log: Loggers.network, type: .info, keys.joined(separator: ", "))
             } else {
-                os_log("Failed to list keypairs. %s", log: Loggers.network, type: .error, self.describeError(error))
+                os_log("Failed to list keypairs. %{public}s", log: Loggers.network, type: .error,
+                       self.describeError(error))
             }
 
             completion(response, error)
@@ -117,13 +122,13 @@ struct IPFSWebserviceAPI: IPFSAPI {
     }
 
     func removeKey(arg: String, completion: @escaping ((_ response: RemoveKeyResponse?, _ error: Error?) -> Void)) {
-        os_log("Removing key %s", log: Loggers.network, type: .info, arg)
+        os_log("Removing key %{public}s", log: Loggers.network, type: .info, arg)
 
         DefaultAPI.removeKey(arg: arg) { (response, error) in
             if response != nil {
-                os_log("Removed key %s", log: Loggers.network, type: .info, arg)
+                os_log("Removed key %{public}s", log: Loggers.network, type: .info, arg)
             } else {
-                os_log("Failed to removed key %s. %s", log: Loggers.network, type: .info,
+                os_log("Failed to removed key %{public}s. %{public}s", log: Loggers.network, type: .info,
                        arg, self.describeError(error))
             }
 

d5aa7e9423fe4a5925229e29dfeb4d71e11c00f9
Connor Power <connor.power@icloud.com>
2018-01-22 08:22:51 +0100

    Add a HorizonCore project to house the core model functionality

---
+++ HorizonCoreTests/HorizonCoreTests.swift
@@ -0,0 +1,18 @@
+//
+//  HorizonCoreTests.swift
+//  HorizonCoreTests
+//
+//  Created by Connor Power on 22.01.18.
+//  Copyright © 2018 Semantical GmbH & Co. KG. All rights reserved.
+//
+
+import XCTest
+@testable import HorizonCore
+
+class HorizonCoreTests: XCTestCase {
+
+    func testExample() {
+        XCTFail("Implement tests")
+    }
+
+}

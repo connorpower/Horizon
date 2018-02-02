73726c64517e56c31736c6045910df031d52618e
Connor Power <connor.power@icloud.com>
2018-02-02 15:06:48 +0100

    Remove cocoapods files

--- Pods/Target Support Files/Pods-HorizonCore/Pods-HorizonCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HorizonCore : NSObject
-@end
-@implementation PodsDummy_Pods_HorizonCore
-@end
--- Pods/Target Support Files/Pods-horizon-cli/Pods-horizon-cli-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_horizon_cli : NSObject
-@end
-@implementation PodsDummy_Pods_horizon_cli
-@end

31b3da0e4f6d3df0a96825c2b6964acf9ed1dae0
Connor Power <connor.power@icloud.com>
2018-02-23 20:02:45 +0100

    Remove unused event logging code

--- HorizonApp/HorizonApp/Model/ModelFactory.swift
+++ HorizonApp/HorizonApp/Model/ModelFactory.swift
@@ -26,44 +26,7 @@ struct ModelFactory {
         return model
     }
 
-    private func handleEvent(_ event: Event) {
-        switch event {
-        case .errorEvent(let errorEvent):
-            handleErrorEvent(errorEvent)
-        case .syncDidStart:
-            Notifications.broadcastSyncStart()
-        case .syncDidEnd:
-            Notifications.broadcastSyncEnd()
-        case .propertiesDidChange:
-            Notifications.broadcastNewData()
-        case .resolvingReceiveListDidStart(let contact):
-            Notifications.broadcastStatusMessage(
-                "Interplanetary Naming System: Resolving location of \(contact.displayName)...")
-        case .addingFileToIPFSDidStart(let url):
-            Notifications.broadcastStatusMessage(
-                "Interplanetary File System: Adding \(url.path)...")
-        case .addingProvidedFileListToIPFSDidStart(let contact):
-            Notifications.broadcastStatusMessage(
-                "Interplanetary File System: Uploading file list for \(contact.displayName)...")
-        case .publishingFileListToIPNSDidStart(let contact):
-            Notifications.broadcastStatusMessage(
-                "Interplanetary Naming System: Publishing file list for \(contact.displayName)...")
-        case .downloadingReceiveListDidStart(let contact):
-            Notifications.broadcastStatusMessage(
-                "Interplanetary File System: Downloading file list from \(contact.displayName)...")
-        case .processingReceiveListDidStart(let contact):
-            Notifications.broadcastStatusMessage(
-                "Interplanetary File System: Processing file list from \(contact.displayName)...")
-        case .keygenDidStart:
-            break
-        case .listKeysDidStart:
-            break
-        case .removeKeyDidStart:
-            break
-        case .renameKeyDidStart:
-            break
-        }
-    }
+    private func handleEvent(_ event: Event) {}
 
     private func handleErrorEvent(_ error: HorizonError) {}
 

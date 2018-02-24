7743474efd5a947269a072091ffc10f9da028669
Connor Power <connor.power@icloud.com>
2018-02-24 19:58:47 +0100

    Print less verbose messages when autostarting the daemon

--- Sources/horizon/DaemonManager+Extension.swift
+++ Sources/horizon/DaemonManager+Extension.swift
@@ -11,16 +11,17 @@ import HorizonCore
 extension DaemonManager {
 
     func startDaemonIfNecessary(_ configuration: ConfigurationProvider) -> Bool {
+        setbuf(__stdoutp, nil)
+
         switch DaemonManager().status(for: configuration) {
         case .pidFilePresentButDaemonNotRunning, .stopped:
-            print("Horizon daemon not running. Auto-starting...")
+            print("Horizon daemon not running. Auto-starting just for this command...")
             do {
                 try DaemonManager().startDaemon(for: configuration)
             } catch {
                 print("Failed to start daemon.")
                 exit(EXIT_FAILURE)
             }
-            print("Started 🤖")
             return true
         default:
             return false
@@ -30,11 +31,8 @@ extension DaemonManager {
     func stopDaemonIfNecessary(_ configuration: ConfigurationProvider) {
         switch DaemonManager().status(for: configuration) {
         case .running:
-            print("Stopping daemon...")
-            if DaemonManager().stopDaemon(for: configuration) {
-                print("Stopped 🤖")
-            } else {
-                print("Failed to stop daemon after auto-starting.")
+            if !DaemonManager().stopDaemon(for: configuration) {
+                print(" Failed to stop daemon after auto-starting.")
                 exit(EXIT_FAILURE)
             }
         default:

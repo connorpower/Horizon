2b6c5e64336c0e70db575887614a33f4e184a112
Connor Power <connor.power@icloud.com>
2018-03-02 14:48:50 +0100

    Run the SPM initialization script as a build dependency

--- spm-init.sh
+++ spm-init.sh
@@ -1,12 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+#  spm-init.sh
+#  -----------
+#  Created by Connor Power on 02.03.18.
+#  Copyright © Connor Power. All rights reserved.
+#
 
 set -x
 set -e
+
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )"; pwd)"
 
 if ! which swift > /dev/null; then
     echo "warning: swift is not installed."
     exit 1
 fi
 
-swift package update && swift package generate-xcodeproj --xcconfig-overrides=Configuration.xcconfig
+swift package update && swift package generate-xcodeproj --xcconfig-overrides="${SCRIPT_DIR}/Configuration.xcconfig"
 
